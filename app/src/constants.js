1d8b5fda63ad10bd1579e9b693e23a0984cbc473
jmrodriguezc <jmrodriguezc1977@gmail.com>
2024-07-17 16:39:25 +0200

    fixing bug with env

--- app/src/constants.js
+++ app/src/constants.js
@@ -3,10 +3,9 @@
 */
 
 
-export const HOST_IP = process.env.REACT_APP_HOST_IP || 'localhost';
+export const BACKEND_HOST_IP = process.env.REACT_APP_HOST_IP || 'localhost';
 export const BACKEND_PORT = process.env.REACT_APP_PORT_CORE || 8080;
-export const BACKEND_BASE = `http://${HOST_IP}`;
-export const BACKEND_URL = `${BACKEND_BASE}:${BACKEND_PORT}`;
+export const BACKEND_URL = `http://${BACKEND_HOST_IP}:${BACKEND_PORT}`;
 
 
 export const MAX_FILE_SIZE = 10 * 1024 * 1024 * 1024; // 10Gb
