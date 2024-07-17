c8aa77875a03cbc0c0bfcae1ad7ec65bc7ed1203
jmrodriguezc <jmrodriguezc1977@gmail.com>
2024-07-17 16:23:31 +0200

    fixing bu with env

--- app/src/constants.js
+++ app/src/constants.js
@@ -3,10 +3,10 @@
 */
 
 
-export const HOST_IP = process.env.HOST_IP || 'localhost';
+export const HOST_IP = process.env.REACT_APP_HOST_IP || 'localhost';
+export const BACKEND_PORT = process.env.REACT_APP_PORT_CORE || 8080;
 export const BACKEND_BASE = `http://${HOST_IP}`;
-export const BACKEND_PORT = process.env.PORT_APP || 3000;
 export const BACKEND_URL = `${BACKEND_BASE}:${BACKEND_PORT}`;
 
 
-export const MAX_FILE_SIZE = 10 * 1024 * 1024 * 1024;
+export const MAX_FILE_SIZE = 10 * 1024 * 1024 * 1024; // 10Gb
